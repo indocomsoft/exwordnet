1a42cde743dfb3d81dfa751d0201eab1342f9f0c
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-03-30 20:13:53 +0800

    Bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExWordNet.MixProject do
   def project do
     [
       app: :exwordnet,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
