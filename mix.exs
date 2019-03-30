c85310a1df70313893188345bccb41770bc0e161
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-03-30 18:28:46 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,21 @@ defmodule ExWordNet.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      source_url: "https://github.com/indocomsoft/exwordnet",
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ],
+      package: package(),
+      description: description()
     ]
   end
 
@@ -21,8 +35,22 @@ defmodule ExWordNet.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:plug_cowboy, "~> 2.0", only: :test},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description do
+    "A library to read WordNet dataset"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/indocomsoft/exwordnet"}
     ]
   end
 end
